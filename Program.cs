b83cf27b258e74dd08d02768ec796189b845ebd7
Vlad <vlad@example.com>
2025-10-03 19:19:30 +0300

    feat: correct comments again

--- Program.cs
+++ Program.cs
@@ -9,34 +9,28 @@ class Program
 {
     static string ProcessData(string dataName)
     {
-        Console.WriteLine($"[SYNC] Начало обработки '{dataName}'...");
         Thread.Sleep(3000);
-
-        string result = $"Обработка '{dataName}' завершена за 3 секунды";
-        Console.WriteLine($"[SYNC] {result}");
-        return result;
+        
+        return $"Обработка '{dataName}' завершена за 3 секунды";
     }
 
     static async Task<string> ProcessDataAsync(string dataName)
     {
-        Console.WriteLine($"[ASYNC] Начало обратоки '{dataName}'...");
         await Task.Delay(3000);
 
-        string result = $"Обработка '{dataName}' завершена за 3 секунды";
-        Console.WriteLine($"[ASYNC] {result}");
-        return result;
+        return $"Обработка '{dataName}' завершена за 3 секунды";
     }
 
     static async Task Main(string[] args)
     {
         Console.WriteLine("=== Демонстрация синхронной и асинхронный обработки ===");
 
-        Console.WriteLine("-- 1. Синхронная оюработка --");
+        Console.WriteLine("-- 1. Синхронная обработка --");
         Stopwatch syncWatch = Stopwatch.StartNew();
 
-        ProcessData("FILE1");
-        ProcessData("FILE2");
-        ProcessData("FILE3");
+        Console.WriteLine(ProcessData("FILE1"));
+        Console.WriteLine(ProcessData("FILE2"));
+        Console.WriteLine(ProcessData("FILE3"));
 
         syncWatch.Stop();
         Console.WriteLine($"\nОбщее время синхронной обработки: {syncWatch.ElapsedMilliseconds / 1000.0} секунд.");
@@ -48,11 +42,12 @@ class Program
         Console.WriteLine("-- 2. Acинхронная оюработка --");
         Stopwatch asyncWatch = Stopwatch.StartNew();
 
-        Task task1 = ProcessDataAsync("FILE1");
-        Task task2 = ProcessDataAsync("FILE1");
-        Task task3 = ProcessDataAsync("FILE1");
+        Task<string> task1 = ProcessDataAsync("FILE1");
+        Task<string> task2 = ProcessDataAsync("FILE2");
+        Task<string> task3 = ProcessDataAsync("FILE3");
 
         await Task.WhenAll(task1, task2, task3);
+        Console.WriteLine(task1.Result + "\n" + task2.Result + "\n" + task3.Result);
 
         asyncWatch.Stop();
 
