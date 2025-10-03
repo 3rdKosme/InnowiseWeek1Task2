8aa701bdd0b641adbcb3b68e7aba6340b15781be
Vlad <vlad@example.com>
2025-10-03 17:46:53 +0300

    feat: done task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo;
+
+class Program
+{
+    static void ProcessData(string dataName)
+    {
+        Console.WriteLine($"[SYNC] Начало обработки '{dataName}'...");
+        Thread.Sleep(3000);
+
+        Console.WriteLine($"[SYNC] Обработка '{dataName}' завершена за 3 секунды");
+        
+    }
+
+    static async Task ProcessDataAsync(string dataName)
+    {
+        Console.WriteLine($"[ASYNC] Начало обратоки '{dataName}'...");
+        await Task.Delay(3000);
+
+        Console.WriteLine($"[ASYNC] Обработка '{dataName}' завершена за 3 секунды");
+    }
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("=== Демонстрация синхронной и асинхронный обработки ===");
+
+        Console.WriteLine("-- 1. Синхронная оюработка --");
+        Stopwatch syncWatch = Stopwatch.StartNew();
+
+        ProcessData("FILE1");
+        ProcessData("FILE2");
+        ProcessData("FILE3");
+
+        syncWatch.Stop();
+        Console.WriteLine($"\nОбщее время синхронной обработки: {syncWatch.ElapsedMilliseconds / 1000.0} секунд.");
+
+        Console.WriteLine("Нажмите любую клавишу для запуска асинхронной обработки...");
+        Console.ReadKey();
+        Console.WriteLine();
+
+        Console.WriteLine("-- 2. Acинхронная оюработка --");
+        Stopwatch asyncWatch = Stopwatch.StartNew();
+
+        Task task1 = ProcessDataAsync("FILE1");
+        Task task2 = ProcessDataAsync("FILE1");
+        Task task3 = ProcessDataAsync("FILE1");
+
+        await Task.WhenAll(task1, task2, task3);
+
+        asyncWatch.Stop();
+
+        Console.WriteLine($"\nОбщее время aсинхронной обработки: {asyncWatch.ElapsedMilliseconds / 1000.0} секунд.");
+        Console.WriteLine($"Разница в {syncWatch.ElapsedMilliseconds / (double) asyncWatch.ElapsedMilliseconds} раз");
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
